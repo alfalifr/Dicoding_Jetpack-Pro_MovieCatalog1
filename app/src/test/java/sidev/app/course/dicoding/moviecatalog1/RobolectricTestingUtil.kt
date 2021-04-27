5dac4754fd98da20ed8e47a9c6ad5b3ab3945959
Aliffiro <fathf48@gmail.com>
2021-04-27 20:59:50 +0700

    Cleaning up

--- app/src/test/java/sidev/app/course/dicoding/moviecatalog1/RobolectricTestingUtil.kt
+++ app/src/test/java/sidev/app/course/dicoding/moviecatalog1/RobolectricTestingUtil.kt
@@ -17,49 +17,7 @@ import androidx.test.espresso.matcher.ViewMatchers as AndroidViewMathcers
 import androidx.test.espresso.action.ViewActions as AndroidViewActions
 
 object RobolectricTestingUtil {
-    fun RecyclerView.setupView(){
-        measure(0, 0)
-        layout(0, 0, 100, 10000)
-    }
-    object ViewActions {
-        fun clickAndBefore(f: () -> Unit): ViewAction {
-            val action = AndroidViewActions.click()
-            return object : ViewAction by action {
-                /**
-                 * Performs this action on the given view.
-                 *
-                 * @param uiController the controller to use to interact with the UI.
-                 * @param view the view to act upon. never null.
-                 */
-                override fun perform(uiController: UiController?, view: View?) {
-                    f()
-                    action.perform(uiController, view)
-                }
-            }
-        }
-    }
     object ViewMatchers {
-        fun isNotDisplayed(): Matcher<View> {
-            val matcher = AndroidViewMathcers.isDisplayed()
-            return object: TypeSafeMatcher<View>(){
-                override fun describeTo(description: Description?) {
-                    description?.appendText("is NOT displayed on the screen to the user")
-                }
-                override fun matchesSafely(item: View?): Boolean = !matcher.matches(item)
-            }
-        }
-        fun isDisplayedAndMatches(predicate: (View) -> Boolean): Matcher<View> {
-            val matcher = AndroidViewMathcers.isDisplayed()
-            return object: Matcher<View> by matcher {
-                override fun matches(item: Any?): Boolean = matcher.matches(item) && predicate(item as View)
-            }
-        }
-        fun textMatches(predicate: (String) -> Boolean): Matcher<View> = object: TypeSafeMatcher<View>() {
-            override fun describeTo(description: Description?) {
-                description?.appendText("with text matches predicate()")
-            }
-            override fun matchesSafely(item: View?): Boolean = item is TextView && predicate(item.text.toString())
-        }
         fun textMatchesAndDisplayed(predicate: (String) -> Boolean): Matcher<View> = object: TypeSafeMatcher<View>() {
             val displayedMatcher =  AndroidViewMathcers.isDisplayed()
             override fun describeTo(description: Description?) {
@@ -67,40 +25,6 @@ object RobolectricTestingUtil {
             }
             override fun matchesSafely(item: View?): Boolean =
                 displayedMatcher.matches(item) && item is TextView && predicate(item.text.toString())
-        }
-    }
-    object RecyclerViewAssertion {
-        private fun templateChildAssertion(body: (RecyclerView) -> Unit): ViewAssertion =
-            ViewAssertion { view, noViewFoundException ->
-                if(noViewFoundException != null)
-                    throw noViewFoundException
-
-                assert(view is RecyclerView)
-                body(view as RecyclerView)
-            }
-
-        fun isChildInPositionDisplayed(
-            childPos: Int, matchers: Matcher<View>
-        ): ViewAssertion = templateChildAssertion {
-            val lm = it.layoutManager
-            assertNotNull(lm)
-            //Impossibly null, so IT IS SAFE to use !!
-            // If lm is null, then below line WILL NEVER be executed because of assertion.
-            val v = lm!!.findViewByPosition(childPos)
-            assert(matchers.matches(v))
-        }
-
-        fun isChildIdInPositionDisplayed(
-            childPos: Int,
-            @IdRes childId: Int,
-            matchers: Matcher<View>,
-        ): ViewAssertion = templateChildAssertion {
-            val lm = it.layoutManager
-            assertNotNull(lm)
-            //Impossibly null, so IT IS SAFE to use !!
-            // If lm is null, then below line WILL NEVER be executed because of assertion.
-            val v = lm!!.findViewByPosition(childPos)?.findViewById<View>(childId)
-            assert(matchers.matches(v))
         }
     }
 }
